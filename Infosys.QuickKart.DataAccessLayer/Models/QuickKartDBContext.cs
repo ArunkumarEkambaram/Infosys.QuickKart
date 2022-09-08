87e2bff704ffe6850f56264bc24c9bddbdff2915
ArunKumar <arunkumar.e@outlook.com>
2022-09-08 17:22:26 +0530

    Implemented ASP.NET Core Web API

--- Infosys.QuickKart.DataAccessLayer/Models/QuickKartDBContext.cs
+++ Infosys.QuickKart.DataAccessLayer/Models/QuickKartDBContext.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 // Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
 // If you have enabled NRTs for your project, then un-comment the following line:
@@ -24,9 +26,13 @@ namespace Infosys.QuickKart.DataAccessLayer.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
+            var config = builder.Build();//Configure the above changes
+            var connectionString = config.GetConnectionString("QuickKartConStr");            
+
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=QuickKartDB;Integrated Security=True;");
+                optionsBuilder.UseSqlServer(connectionString);
             }
         }
 
